9c0828abbc057beeff44612c6d42615be8e8f705
Robin-1998 <10616@holbertonstudents.com>
2025-04-07 17:59:53 +0200

    ajout du fichier pour la tache 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf -Vérifie si un noeudest une feuille
+ * @node: pointeurvers le noeudà vérifier
+ * Return: on retourne 1 si c'est une feuille, sinon 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	/* Si node est pas NULL on retourne 0*/
+
+	/* Vérifie si le nœud n'a pas d'enfants à gauche et à droite */
+	if (node->right == NULL && node->left == NULL)
+	{
+	/* C'est une feuille, retourne 1 */
+		return(1);
+	}
+	/* Sinon, ce n'est pas une feuille */
+	return (0);
+}
+
