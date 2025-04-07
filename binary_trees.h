6046d9758c98f4445e80b19fb59df7dfe2f749eb
Robin-1998 <10616@holbertonstudents.com>
2025-04-07 16:51:22 +0200

    ajout du fichier pour la task 3

--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,8 @@
 #include <stdio.h>     /* Entrée/sortie standard */
 #include <stdlib.h>    /* Allocation mémoire, conversions, process */
 #include <string.h>    /* Manipulations de chaînes de caractères */
+#include<malloc.h>
+
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -30,7 +32,7 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
 
