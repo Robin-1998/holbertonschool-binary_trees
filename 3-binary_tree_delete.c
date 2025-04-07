0b310db05fb4933551ddc744f0b10d221c61e087
Robin-1998 <10616@holbertonstudents.com>
2025-04-07 16:56:00 +0200

    Correctif betty pour la tâche 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,20 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - e
- * @param tree
+ * binary_tree_delete - Dézinguer l'arbre binaire
+ * @tree: arbre binaire à dézinguer
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-
+	/* Vérifie si l'arbre est vide */
 	if (tree == NULL)
 	{
 		return;
 	}
 
+	/* Supprime récursivement le sous-arbre gauche */
 	binary_tree_delete(tree->left);
+	/* Supprime récursivement le sous-arbre droit */
 	binary_tree_delete(tree->right);
 
+	/* Libère la mémoire allouée pour ce nœud */
 	free(tree);
 }
 
