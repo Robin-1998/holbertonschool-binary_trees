d70297c3d051c8a489f11cb6b6258e160134b52e
Robin-1998 <10616@holbertonstudents.com>
2025-04-08 09:07:18 +0200

    Ajout du fichier pour la task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,5 +1,18 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - fonction qui ajoute le noeud sur la racine
+ * @node: correspond à notre noeud
+ * Return: on retourne 1 si le noeud est dans la racine et si nonon retourne 0
+ */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
+	if (node == NULL)
+		return (0);
 
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }
