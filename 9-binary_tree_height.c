c2e2b6cc9b0f6827b48c89bba95ebd30c5c69cd6
Robin-1998 <10616@holbertonstudents.com>
2025-04-08 13:24:42 +0200

    ajout du fichier pour la tache 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_height - fonction qui mesure la hauteur d'un arbre binaire
  * @tree: Correspond à notre arbre binaire
- * Return: on retourn la hauteur de l'arbre
+ * Return: on retourne la hauteur de l'arbre
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -12,14 +12,19 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int hauteur_left;
 	int hauteur_right;
 
-	if (tree == NULL)
-	{
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{ /* si l'arbre est vide ou sans enfant, hauteur = 0 */
 		return (0);
 	}
 	hauteur_left = binary_tree_height(tree->left);
+	/* appel récursif pour mesurer la hauteur du sous-arbre gauche */
 	hauteur_right = binary_tree_height(tree->right);
+	/* appel récursif pour mesurer la hauteur du sous-arbre droit */
+
 	if (hauteur_left > hauteur_right)
+/* si gauche plus haut que droite, on ajoute 1 */
 		return (hauteur_left + 1);
 	else
 		return (hauteur_right + 1);
+		/* sinon, droite plus haute ou égale, on ajoute 1 */
 }
