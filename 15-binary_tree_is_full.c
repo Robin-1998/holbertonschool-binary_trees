cb0fda521ffdc63cb42c64e4928bf566de52f083
Robin-1998 <10616@holbertonstudents.com>
2025-04-09 15:30:17 +0200

    ajout d'un fichier pour la tache 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,23 +2,30 @@
 /**
  * binary_tree_is_full -fonction qui vérifiesi un arbre binaire est complet
  * @tree: correspond à notre arbre binaire
- * Return: int
+ * Return: on retourne 1 si c'est full et 0 si ça ne l'est pas
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	/* Si l'arbre est NULL, on considère qu'il n'est pas plein */
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL && tree->right != NULL)
-	{
+	/* Si le nœud est une feuille (pas d'enfants), il est plein */
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	}
 
-	if ((tree->left == NULL || tree->right == NULL))
-	{
-		return (0);
-	}
+	/* Si le nœud a deux enfants, on vérifie récursivement que*/
+	/* les deux sous-arbres sont pleins */
+	if ((tree->left != NULL) && (tree->right != NULL))
 
-	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+		/*int left = binary_tree_is_full(tree->left);*/
+		/*int right = binary_tree_is_full(tree->right);*/
+			/*return (left && right);*/
+
+		return (binary_tree_is_full(tree->left)
+		&& binary_tree_is_full(tree->right));
+
+/* Si le nœud a un seul enfant, il ne respecte pas la définition*/
+/* d’un arbre plein */
+	return (0);
 }
-
